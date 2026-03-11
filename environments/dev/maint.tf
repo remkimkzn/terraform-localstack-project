059988061d0ced6cba0d4236d0dd604120683649
Rem <rem@Magic>
2026-03-12 02:33:50 +0300

    dynamodb

--- environments/dev/maint.tf
+++ environments/dev/maint.tf
@@ -1,6 +1,11 @@
 # вызываем моуль s3 и задаем имя bucket
+# подключили модуль DynamoDB  NoSQL
 module "storage" {
   source      = "../../modules/s3"
   bucket_name = "terraform-localstack-bucket"
 }
 
+module "database" {
+  source     = "../../modules/dynamodb"
+  table_name = "terraform-localstack-table"
+}
